89c76d839f7a4f1093d819c4c312ae72169ba440
Yehor Kravchenko <brazuca.egor@gmail.com>
2024-01-23 22:43:16 +0200

    DEV: Initial commit

---
+++ src/main/java/com/kravchenko/accounts/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.kravchenko.accounts.repository;
+
+import com.kravchenko.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+}
