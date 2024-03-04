076aa8a0a2894cb2073db6feef6450a03468e31e
Yehor Kravchenko <brazuca.egor@gmail.com>
2024-03-04 22:14:34 +0200

    DEV: Demo of implementing RateLimiter pattern in accounts-ms for single method

--- src/main/java/com/kravchenko/accounts/controller/AccountsController.java
+++ src/main/java/com/kravchenko/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.kravchenko.accounts.dto.CustomerDto;
 import com.kravchenko.accounts.dto.ErrorResponseDto;
 import com.kravchenko.accounts.dto.ResponseDto;
 import com.kravchenko.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -228,11 +229,19 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    private ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        logger.debug("getJavaVersionFallback() method invoked...");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
