ddddd5a97ba9ef7885d16db431f8d5ec9338a9a1
Yehor Kravchenko <brazuca.egor@gmail.com>
2024-03-11 23:27:15 +0200

    DEV: Implement OpenTelemetry changes inside accounts

--- src/main/java/com/kravchenko/accounts/controller/CustomerController.java
+++ src/main/java/com/kravchenko/accounts/controller/CustomerController.java
@@ -60,8 +60,9 @@ public class CustomerController {
                                                                    @RequestParam
                                                                    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
                                                                    String mobileNumber) {
-        logger.debug("eazyBank-correlation-id found: {} ", correlationId);
+        logger.debug("fetchCustomerDetails started");
         CustomerDetailsDto customerDetailsDto = iCustomersService.fetchCustomerDetails(mobileNumber, correlationId);
+        logger.debug("fetchCustomerDetails ended");
         return ResponseEntity.status(HttpStatus.SC_OK).body(customerDetailsDto);
 
     }
